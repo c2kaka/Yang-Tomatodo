3f3d7a0f25d3616718f13c40ddf7e50041ea37e7
“c2kaka <c2kaka@126.com>
2019-06-28 13:54:55 +0800

    完成登录页面

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,10 +1,62 @@
 import React from "react";
+import {Input, Icon, Button} from "antd";
+import axios from "src/config/axios";
+import {Link} from "react-router-dom";
+import './Login.scss'
 
-class Login extends React.Component {
+interface ILoginState {
+    account: string;
+    password: string;
+}
+
+class Login extends React.Component <any, ILoginState> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            account: '',
+            password: '',
+        };
+    }
+
+    onChange = (key: keyof ILoginState, value: string) => {
+        const newState = {};
+        newState[key] = value;
+        this.setState(newState);
+    };
+
+    submit = async () => {
+        const {account, password} = this.state;
+        try {
+            await axios.post('sign_in/user', {
+                account,
+                password,
+            });
+            this.props.history.push('/');
+        } catch (e) {
+            throw new Error(e);
+        }
+    };
+
     render() {
+        const {account, password} = this.state;
         return (
-            <div>
-                Login
+            <div className="Login" id="Login">
+                <h1>番茄闹钟登录</h1>
+                <Input
+                    placeholder="请输入用户名"
+                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
+                    allowClear={true}
+                    value={account}
+                    onChange={e => this.onChange("account", e.target.value)}
+                />
+                <Input.Password
+                    value={password}
+                    placeholder="请输入密码"
+                    onChange={e => this.onChange("password", e.target.value)}
+                />
+
+                <Button type='primary' className='loginBtn' onClick={this.submit}>登录</Button>
+                <p>还没有账号？立即<Link to='/signUp'>注册</Link></p>
             </div>
         )
     }
--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import {Input , Icon , Button} from "antd";
-import  axios from "src/config/axios";
+import {Input, Icon, Button} from "antd";
+import axios from "src/config/axios";
 import {Link} from "react-router-dom";
 import './SignUp.scss'
 
@@ -10,67 +10,61 @@ interface ISignUpState {
     password_confirmation: string;
 }
 
-class SignUp extends React.Component <any,ISignUpState>{
-    constructor(props){
+class SignUp extends React.Component <any, ISignUpState> {
+    constructor(props) {
         super(props);
-        this.state={
+        this.state = {
             account: '',
             password: '',
             password_confirmation: ''
         };
     }
 
-    onChangeAccount = (e)=>{
-        this.setState({account: e.target.value});
+    onChange = (key:keyof ISignUpState, value:string) => {
+        const newState = {};
+        newState[key] = value;
+        this.setState(newState);
     };
 
-    onChangePassword = (e)=>{
-        this.setState({password: e.target.value});
-    };
-
-    onChangeConfirmation = (e)=>{
-        this.setState({password_confirmation: e.target.value});
-    };
-
-    submit = async ()=>{
-        const { account ,password ,password_confirmation }=this.state;
-        try{
-            await axios.post('sign_up/user',{
+    submit = async () => {
+        const {account, password, password_confirmation} = this.state;
+        try {
+            await axios.post('sign_up/user', {
                 account,
                 password,
                 password_confirmation
             });
-        }
-        catch (e) {
+            this.props.history.push('/');
+        } catch (e) {
             throw new Error(e);
         }
     };
 
     render() {
-        const { account ,password ,password_confirmation }=this.state;
+        const {account, password, password_confirmation} = this.state;
         return (
             <div className="SignUp" id="SignUp">
                 <h1>番茄闹钟注册</h1>
                 <Input
-                    placeholder="请输入你的用户名"
-                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }}/>}
+                    placeholder="请输入用户名"
+                    prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>}
                     allowClear={true}
                     value={account}
-                    onChange={this.onChangeAccount}
+                    onChange={e => this.onChange("account",e.target.value)}
                 />
                 <Input.Password
                     value={password}
                     placeholder="请输入密码"
-                    onChange={this.onChangePassword}
+                    onChange={e => this.onChange("password",e.target.value)}
                 />
                 <Input.Password
                     value={password_confirmation}
                     placeholder="请确认密码"
-                    onChange={this.onChangeConfirmation}
+                    onChange={e => this.onChange("password_confirmation",e.target.value)}
                 />
 
                 <Button type='primary' className='signUpBtn' onClick={this.submit}>注册</Button>
-                <p>已有账号?<Link to='/login' >登录</Link></p>
+                <p>已有账号?<Link to='/login'>登录</Link></p>
             </div>
         )
     }
